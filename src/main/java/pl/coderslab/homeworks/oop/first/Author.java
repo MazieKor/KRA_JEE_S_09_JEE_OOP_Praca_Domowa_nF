cf032b654b7a80e393a9ca2c07555f6f5b8a78a1
Maciej Korczala <maciej.coders@gmail.com>
2021-03-25 14:39:30 +0100

    second commit

--- src/main/java/pl/coderslab/homeworks/oop/first/Author.java
+++ src/main/java/pl/coderslab/homeworks/oop/first/Author.java
@@ -1,2 +1,25 @@
-package pl.coderslab.homeworks.oop.first;public class Author {
+package pl.coderslab.homeworks.oop.first;
+
+public class Author {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String pseudonym;
+
+    public Author(int id, String firstName, String lastName, String pseudonym){
+
+    }
+        public void setId(int id){
+            this.id = id;
+        }
+
+    public static void main(String[] args) {
+        Book book1;
+        String book1Name = new Book().getTitle();
+        System.out.println("pierwszy String: " + book1Name);
+
+    }
+
+
+
 }
