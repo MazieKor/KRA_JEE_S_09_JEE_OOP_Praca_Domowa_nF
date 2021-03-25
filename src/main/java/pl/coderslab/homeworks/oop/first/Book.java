1a6cd819e80b9bed03241baea09ef5836e3f09fb
Maciej Korczala <maciej.coders@gmail.com>
2021-03-25 14:46:21 +0100

    Third commit - execrcises

--- src/main/java/pl/coderslab/homeworks/oop/first/Book.java
+++ src/main/java/pl/coderslab/homeworks/oop/first/Book.java
@@ -1,2 +1,38 @@
-package pl.coderslab.homeworks.oop.first;public class Book {
+package pl.coderslab.homeworks.oop.first;
+
+public class Book {
+    private int id;
+    private String title;
+    private boolean available = true;
+    private Author author;
+    private Author[] additionalAuthors;
+
+//    public Book (int id, String title){
+//    }
+//    public Book (int id, String title, Author author){
+//    }
+//    public Book (int id, String title, Author author, Author[] additionalAuthors){
+//    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+//    public Author setAuthor(int id){
+//        author.id = setId(id);
+//    }
+
+
 }
